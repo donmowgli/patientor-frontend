b2b44e66b5ef1be26962b847ed76caeb2ba04122
donmowgli <akseli.jaara@gmail.com>
2022-05-30 16:59:37 +0300

    refactoring for working patient adding and rendering of diagnosis

--- src/PatientPage/index.tsx
+++ src/PatientPage/index.tsx
@@ -2,20 +2,37 @@ import React, { useState } from "react";
 import axios from 'axios';
 import { useStateValue } from "../state";
 import { Typography } from "@material-ui/core";
-import { Patient } from "../types";
+import { Patient, Diagnosis, Entry } from "../types";
 import { apiBaseUrl } from "../constants";
 
 const PatientPage = () => {
-    const [{ patient }] = useStateValue();
+    const [{ patient, diagnoses }] = useStateValue();
     const [sPatient, setsPatient] = useState(patient);
+    const [patientDiagnoses, setPatientDiagnoses] = useState(diagnoses);
 
-    const getInfo = async () => {
-        await axios.get<Patient>(`${apiBaseUrl}/patients/${patient.id}`)
-                    .then(response => {
-                        setsPatient(response.data);
-                    });
-    };
-    void getInfo();
+    React.useEffect(() => {
+        const getInfo = async () => {
+            await axios.get<Patient>(`${apiBaseUrl}/patients/${patient.id}`)
+                        .then(response => {
+                            setsPatient(response.data);
+                        });
+        };
+        void getInfo();
+    }, [patient]);
+
+    React.useEffect(() => {
+        const setNewPatientDiagnoses = () => {
+            const newDiagnoses: Array<Diagnosis> = [];
+            sPatient.entries?.forEach(entry => {entry.diagnosisCodes?.forEach(code => {
+                Object.values(diagnoses).forEach(diagnose => {
+                    if(diagnose.code === code) newDiagnoses.push(diagnose);
+                });
+            });
+            setPatientDiagnoses({...newDiagnoses as any});
+        });
+        };
+        void setNewPatientDiagnoses();
+    }, [sPatient]);
 
     return (
         <div className="App">
@@ -25,6 +42,19 @@ const PatientPage = () => {
             </Typography>
             <p>SSN: {sPatient.ssn}</p>
             <p>Occupation: {sPatient.occupation}</p>
+            <Typography align="left" variant="h6">
+                Entries
+            </Typography>
+            <ul>{sPatient.entries?.map((entry : Entry) => (
+                <div key={entry.id}>
+                    <li>{entry.date} {entry.description}</li>
+                    <ul>{Object.values(patientDiagnoses).map((diagnosis : Diagnosis) => (
+                        <li key={diagnosis.name}>{diagnosis.code} {diagnosis.name}</li>
+                    ))}
+                    </ul>
+                </div>
+                ))}
+            </ul>
         </div>
     );
 };
--- src/types.ts
+++ src/types.ts
@@ -17,4 +17,49 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: Array<Entry>;
 }
+
+interface baseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+interface healthCheckEntry extends baseEntry {
+  type: "HealthCheck";
+  healthCheckRating: healthCheckRating;
+}
+
+interface occupationalHealthcareEntry extends baseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: sickLeave;
+}
+
+interface hospitalEntry extends baseEntry{
+  type: "Hospital";
+  discharge: discharge;
+}
+
+interface sickLeave {
+  startDate: string;
+  endDate: string
+}
+
+interface discharge {
+  date: string;
+  criteria: string;
+}
+
+enum healthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "highRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+export type Entry = baseEntry | healthCheckEntry | occupationalHealthcareEntry | hospitalEntry ;
+
